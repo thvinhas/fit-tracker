1e39c2b73f97ce8a36d639bf5864b7db92242e2b
Thiago Vinhas <vinhas.thiago@gmail.com>
2026-05-20 23:47:09 +0100

    change icon

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -78,7 +78,7 @@ define(['./workbox-bbbdf4d3'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "/index.html",
-    "revision": "0.rj5ncsdrks8"
+    "revision": "0.7ct9dgole94"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
